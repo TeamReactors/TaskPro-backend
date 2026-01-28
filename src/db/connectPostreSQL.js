c0b5a7fc936bddf7e9cca9246ab3d3052933fe74
ilkerthedev <ilkerselimen@gmail.com>
2026-01-29 01:11:17 +0300

    fix: postgre connection

--- src/db/connectPostreSQL.js
+++ src/db/connectPostreSQL.js
@@ -10,11 +10,12 @@ export const sql = neon(`postgresql://${user}:${password}@${host}/${database}`);
 
 export const connectPostreSQL = async () => {
   try {
+    // email unique keyword 
     await sql`
             CREATE TABLE IF NOT EXISTS users(
                 id SERIAL PRIMARY KEY,
                 name VARCHAR(100) NOT NULL,
-                email VARCHAR(100) NOT NULL,
+                email VARCHAR(100) NOT NULL UNIQUE, 
                 password VARCHAR(255) NOT NULL
             );
         `;
@@ -35,24 +36,27 @@ export const connectPostreSQL = async () => {
         created_at TIMESTAMP WITH TIME ZONE DEFAULT now()
       );
     `;
-
+    // columns one cekildi position eklendi
+    await sql`
+      CREATE TABLE IF NOT EXISTS columns(
+        id SERIAL PRIMARY KEY,
+        title VARCHAR(100) NOT NULL,
+        board_id INTEGER REFERENCES board(id) ON DELETE CASCADE,
+        position INTEGER DEFAULT 0, 
+        created_at TIMESTAMP WITH TIME ZONE DEFAULT now()
+      );`;
+    // position eklendi
     await sql`
       CREATE TABLE IF NOT EXISTS task(
         id SERIAL PRIMARY KEY,
         title VARCHAR(100) NOT NULL,
         description TEXT,
-        priority VARCHAR(10),
+        priority VARCHAR(20) DEFAULT 'Low', 
         column_id INTEGER REFERENCES columns(id) ON DELETE CASCADE,
         board_id INTEGER REFERENCES board(id) ON DELETE CASCADE,
         deadline TIMESTAMP WITH TIME ZONE,
+        position INTEGER DEFAULT 0,
         created_at TIMESTAMP WITH TIME ZONE DEFAULT now()
-      );`;
-
-    await sql`
-      CREATE TABLE IF NOT EXISTS columns(
-        id SERIAL PRIMARY KEY,
-        title VARCHAR(100) NOT NULL,
-        board_id INTEGER REFERENCES board(id) ON DELETE CASCADE
       );`;
 
     console.log("Bağlantı başarılı");
